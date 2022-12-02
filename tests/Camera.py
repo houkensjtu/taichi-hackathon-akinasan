85f2a4f905cc98e6ae1982a46e43355f3ed59632
houkensjtu <houkensjtu@gmail.com>
2022-12-02 15:12:43 +0800

    Add Camera and Processor class

--- tests/Camera.py
+++ tests/Camera.py
@@ -1,8 +1,45 @@
-class Camera:
-    def __init__(self, dim, fps):
-        self.dim = dim
-        self.fps = fps
-        self.cap = cv.VideoCapture(0)
+import cv2 as cv
+from Processor import Processor
 
-    def start_recording(self, Operation):
+class Camera(object):
+    def __init__(self, resolution, framerate):
+        self.resolution = resolution
+        self.framerate = framerate
+        self.cap = cv.VideoCapture(0)
+        if not self.cap.isOpened():
+            print('Cannot open camera')
+            exit()
+
+    def start_recording(self, Processor):
+        self.processor = Processor()
+        frame_id = 0
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                print('Cannot receive frame. Exiting ...')
+                break
+            filename = f'{frame_id:06d}.jpg'
+            cv.imwrite(filename, cv.resize(frame, self.resolution))
+            print(f'>>> Captured and saved frame {filename}')
+            frame_id += 1
+            if cv.waitKey(1) == ord('q'):
+                break
+
+    def wait_recording(self, time):
         pass
+
+    def stop_recording(self):
+        # self.processor.flush()
+        self.cap.release()
+        cv.destroyAllWindows()
+    
+if __name__ == '__main__':
+    dim = (160, 120)
+    fps = 15
+    camera = Camera(dim, fps)
+    print('Start recording ...')
+    camera.start_recording(Processor)
+    sleep(2)
+
+    camera.stop_recording()
+    print('Finish recording ...')
