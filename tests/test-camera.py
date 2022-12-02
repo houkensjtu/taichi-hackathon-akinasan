2f3a4910087fed85d5d6ba1ae5eec32e65e9735e
houkensjtu <houkensjtu@gmail.com>
2022-12-02 11:26:43 +0800

    Test camera capturing and resizing

--- tests/test-camera.py
+++ tests/test-camera.py
@@ -1,22 +1,27 @@
 import numpy as np
 import cv2 as cv
+
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
+frame_id =0
 while True:
+    dim = (160, 120)    
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    print(f'>>> Current frame width: {cap.get(cv.CAP_PROP_FRAME_WIDTH)}')
-    print(f'>>> Current frame height: {cap.get(cv.CAP_PROP_FRAME_HEIGHT)}')
     # Our operations on the frame come here
     # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Display the resulting frame
     # cv.imshow('frame', gray)
-    cv.imshow('frame', frame)
+    # cv.imshow('frame', frame)
+    filename = f'{frame_id:06d}.jpg'
+    cv.imwrite(filename, cv.resize(frame, dim))
+    print(f'>>> Captured and saved frame {filename}')
+    frame_id += 1
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
